891e23f703f5ae6d66dfe42e945295eb6823b4c1
r00tdr4g0n <parkour0071@gmail.com>
2023-06-16 18:22:17 +0900

    [Mod] 툴바 생성하는 부분 설명 추가

--- TypingViewer/MainFrm.cpp
+++ TypingViewer/MainFrm.cpp
@@ -72,19 +72,24 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	}
 	m_wndStatusBar.SetIndicators(indicators, sizeof(indicators)/sizeof(UINT));
 
+	// 툴바 생성
 	if (!m_wndToolBar.CreateEx(this, TBSTYLE_FLAT, WS_CHILD | WS_VISIBLE | CBRS_TOP | CBRS_TOOLTIPS | CBRS_FLYBY | CBRS_SIZE_DYNAMIC)) {
 		TRACE(L"Failed to create tool bar\n");
 		return -1;
 	}
 
+	// 툴바에 사용할 이미지 가져오기
 	CImageList imgList;
 	CImage img;
 	CBitmap bmp[TOOLBAR_BTN_CNT];
+	// 툴바 버튼 개별적으로 조작하기 위해서 필수
 	CToolBarCtrl& toolbarCtrl = m_wndToolBar.GetToolBarCtrl();
 	CRect rect;
 
+	// 투명한 배경의 이미지를 넣을 수 있는 이미지 리스트 만들기
 	imgList.Create(32, 32, ILC_COLOR24 | ILC_MASK, 4, 4);
 
+	// 이미지들을 비트맵 배열에 저장
 	LoadBmp(bmp[0], m_path + L"\\" + TOOLBAR_FOLDER_PATH);
 	LoadBmp(bmp[1], m_path + L"\\" + TOOLBAR_FONT_PATH);
 	LoadBmp(bmp[2], m_path + L"\\" + TOOLBAR_MONITOR_OFF_PATH);
@@ -92,21 +97,29 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	LoadBmp(bmp[4], m_path + L"\\" + TOOLBAR_SETTING_PATH);
 	LoadBmp(bmp[5], m_path + L"\\" + TOOLBAR_INFO_PATH);
 
+	// 이미지 리스트에 이미지가 저장된 비트맵들을 추가
 	for (int cnt = 0; cnt < TOOLBAR_BTN_CNT; cnt++) {
 		imgList.Add(&bmp[cnt], TOOLBAR_MASK_COLOR);
 	}
 
+	// 툴바 컨트롤에 이미지 리스트 지정
 	toolbarCtrl.SetImageList(&imgList);
 
+	// 이미지 등록을 했으므로 비트맵 해제
 	for (int cnt = 0; cnt < TOOLBAR_BTN_CNT; cnt++) {
 		bmp[cnt].Detach();
 	}
 
+	// 비트맵 해제 후, 이미지 리스트 해제
 	imgList.Detach();
 
+	// 툴바 컨트롤의 버튼 가로 크기 (최소, 최대) 지정
 	toolbarCtrl.SetButtonWidth(100, 150);
 
-	m_wndToolBar.SetButtons(NULL, TOOLBAR_BTN_CNT - 1);
+
+	// 버튼 개수 지정
+	m_wndToolBar.SetButtons(NULL, TOOLBAR_BTN_CNT - 1); // 없어도 잘 됨...
+	// 버튼 정보 입력
 	m_wndToolBar.SetButtonInfo(0, ID_TOOLBAR_BUTTON_SAVE, TBSTYLE_BUTTON | TBSTATE_ENABLED, 0);
 	m_wndToolBar.SetButtonText(0, L"저장");
 	m_wndToolBar.SetButtonInfo(1, ID_TOOLBAR_BUTTON_FONT, TBSTYLE_BUTTON | TBSTATE_ENABLED, 1);
@@ -118,23 +131,27 @@ int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
 	m_wndToolBar.SetButtonInfo(4, ID_TOOLBAR_BUTTON_INFO, TBSTYLE_BUTTON | TBSTATE_ENABLED, 5);
 	m_wndToolBar.SetButtonText(4, L"정보");
 
+	// 각 버튼의 사이즈 변경
 	m_wndToolBar.GetItemRect(0, rect);
 	m_wndToolBar.SetSizes(rect.Size() + CSize(0, 15), CSize(32, 32));
 
+	// 툴바 도킹
 	m_wndToolBar.EnableDocking(CBRS_ALIGN_ANY);
 	EnableDocking(CBRS_ALIGN_ANY);
 	DockControlBar(&m_wndToolBar);
 
 	this->SetWindowTextW(L"Typing Viewer");
 
-	CLoginDlg dlg;
+	/*CLoginDlg dlg;
 
 	if (dlg.DoModal() == IDOK) {
 		m_bLogin = true;
 	}
 	else {
 		m_bLogin = false;
-	}
+	}*/
+
+	m_bLogin = true;
 
 	return 0;
 }
@@ -172,7 +189,7 @@ void CMainFrame::LFtoCF(LOGFONT& a_lf, CHARFORMAT2& a_cf)
 	a_cf.dwEffects = CFM_EFFECTS | CFE_AUTOBACKCOLOR;
 	a_cf.dwEffects &= ~(CFE_PROTECTED | CFE_LINK | CFM_COLOR);
 	a_cf.cbSize = sizeof(CHARFORMAT2);
-	a_cf.yHeight = (abs)((ceil)(a_lf.lfHeight * 72 / (double)GetDeviceCaps(CClientDC(this), LOGPIXELSY))) * 20;
+	a_cf.yHeight = LONG((abs)((ceil)(a_lf.lfHeight * 72 / (double)GetDeviceCaps(CClientDC(this), LOGPIXELSY))) * 20);
 	a_cf.yOffset = 0;
 	if (a_lf.lfWeight < FW_BOLD) a_cf.dwEffects &= ~CFE_BOLD;
 	if (!a_lf.lfItalic) a_cf.dwEffects &= ~CFE_ITALIC;
